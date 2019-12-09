2af59b036a0fc37f330472c50d829fef2600f7fe
Shavauhn Gabay <shavyg2@gmail.com>
2019-12-08 20:56:05 -0500

    added the ability to handle 404s

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-const chain  = require("chain-middleware");
+const chain = require("chain-middleware");
 import is from "@sindresorhus/is";
 
+const Progress = Symbol.for("continue");
 
-
-function Next(req:any,res:any,func:ApiFunction[]){
+function Next(req: any, res: any, func: ApiFunction[]) {
 
   let position = 0;
 
-  function next(err?:any){
+  function next(err?: any) {
     let status = 500;
-    
-    if(err){
+
+    if (err) {
       if (is.primitive(err)) {
         res.status(status).send(err);
       } else if (is.object(err)) {
@@ -19,18 +19,18 @@ function Next(req:any,res:any,func:ApiFunction[]){
         res.status(status).send(err);
       }
       res.status(500).send(err);
-    }else{
+    } else {
       let doing = func[position];
 
-      if(doing){
-        doing(req,res,next);
-      }else{
+      if (doing) {
+        doing(req, res, next);
+      } else {
         res.status(404).json({
-          message:"not found"
+          message: "not found"
         })
       }
     }
-    
+
     position++;
 
   }
@@ -45,21 +45,21 @@ export type ApiFunction = (
 ) => any | Promise<any>;
 
 
-type Params = (ApiFunction|number)[];
+type Params = (ApiFunction | number)[];
 
 export function Api(...funcs: Params) {
 
-  let apis = funcs.filter(x=>is.function_(x)) as ApiFunction[];
-  let stati = funcs.filter(x=>is.number(x)) as number[];
-  const status = is.number(stati[0])?stati[0]:200; 
+  let apis = funcs.filter(x => is.function_(x)) as ApiFunction[];
+  let returnCodes = funcs.filter(x => is.number(x)) as number[];
+  const status = is.number(returnCodes[0]) ? returnCodes[0] : 200;
 
-  const func = apis.pop();
+  const handler = apis.pop();
 
-  const final_function =  (req:any, res: import("express").Response, next:any) => {
+  const apiFunction = (req: any, res: import("express").Response, next: any) => {
     Promise.resolve(
       new Promise((r, j) => {
         try {
-          r(func(req, res, next));
+          r(handler(req, res, next));
         } catch (e) {
           j(e);
         }
@@ -90,103 +90,123 @@ ${e.stack}
 `.trim()
           );
         } else if (is.object(e)) {
-          res.status(500).json(e);
+          const { code, ...data } = e as any;
+          if (is.number(code)) {
+            return res.status(code).json(data);
+          } else {
+            return res.status(500).json(e);
+          }
         } else {
-          res.send(e);
+          return res.send(e);
         }
       });
   };
 
 
-  return (req:any,res:any)=>{
-    return Next(req,res,[...apis,final_function])
+  return (req: any, res: any) => {
+    return Next(req, res, [...apis, apiFunction])
   }
 }
 
 
 const Alt = Api;
 
-export namespace Api{
+export namespace Api {
 
 
-    function makeMethod (method:string){
-        return (...api:Params)=>{
-            const fun:ApiFunction =  (req,res,)=>{
-                if(req.method.toLocaleLowerCase()===method.toLowerCase()){
-                    return Alt(...api)(req,res);
-                }
-            }
-            return fun;
-        } 
-    }
-    export const get = makeMethod("get")
-
-    export const post = makeMethod("post");
-
-    export const put = makeMethod("put");
-
-    export const del = makeMethod("delete");
-
-    export const checkout = makeMethod("checkout");
-
-    export const copy = makeMethod("copy");
-
-    export const head = makeMethod("head");
-
-
-    export const lock = makeMethod("lock");
-
-
-    export const merge = makeMethod("merge");
-
-    export const mkactivity = makeMethod("mkactivity");
-
-
-    export const mkcol = makeMethod("mkcol");
-
-
-    export const move = makeMethod("move");
-
-
-    export const msearch = makeMethod("m-search");
-
-
-    export const notify = makeMethod("notify");
-
-
-    export const options = makeMethod("options");
-
-
-    export const patch = makeMethod("patch");
-
-
-    export const purge = makeMethod("purge")
-
-    export const report = makeMethod("report");
-
-    export const search = makeMethod("search");
-
-
-    export const subscribe = makeMethod("subscribe");
-
-
-    export const trace = makeMethod("trace");
-
-
-    export const unlock = makeMethod("unlock");
-
-    export const unsubscribe = makeMethod("unsubscribe");
-
-
-    export function fold(...params:Params){
-        return (req:any,res:any,next:any)=>{
-            let numbers = params.filter(x=>is.number(x)) ;
-            let funcs = params.filter(x=>is.function_(x)) as ApiFunction[];
-            funcs.forEach(func=>{
-                Alt(func(req,res,next),...numbers);
-            });
+  function makeMethod(method: string) {
+    return (...api: Params) => {
+      const fun: ApiFunction = (req, res) => {
+        if (req.method.toLocaleLowerCase() === method.toLowerCase()) {
+          return Alt(...api)(req, res);
+        } else {
+          return Progress;
         }
+      }
+      return fun;
     }
+  }
+
+
+  export const get = makeMethod("get")
+
+  export const post = makeMethod("post");
+
+  export const put = makeMethod("put");
+
+  export const del = makeMethod("delete");
+
+  export const checkout = makeMethod("checkout");
+
+  export const copy = makeMethod("copy");
+
+  export const head = makeMethod("head");
+
+
+  export const lock = makeMethod("lock");
+
+
+  export const merge = makeMethod("merge");
+
+  export const mkactivity = makeMethod("mkactivity");
+
+
+  export const mkcol = makeMethod("mkcol");
+
+
+  export const move = makeMethod("move");
+
+
+  export const msearch = makeMethod("m-search");
+
+
+  export const notify = makeMethod("notify");
+
+
+  export const options = makeMethod("options");
+
+
+  export const patch = makeMethod("patch");
+
+
+  export const purge = makeMethod("purge")
+
+  export const report = makeMethod("report");
+
+  export const search = makeMethod("search");
+
+
+  export const subscribe = makeMethod("subscribe");
+
+
+  export const trace = makeMethod("trace");
+
+
+  export const unlock = makeMethod("unlock");
+
+  export const unsubscribe = makeMethod("unsubscribe");
+
+
+  export function fold(...params: Params) {
+    return (req: any, res: any, next: any) => {
+      let possibleResultCode = params.filter(x => is.number(x));
+      let funcs = params.filter(x => is.function_(x)) as ApiFunction[];
+      const result = funcs.reduce((result: any, handler) => {
+        const handlerResult = handler(req, res, next);
+        if (handlerResult === Progress) {
+          return Progress
+        } else {
+          return Alt(handlerResult, ...possibleResultCode);
+        }
+      }, Progress);
+
+      if (result === Progress) {
+        res.status(404).json({
+          message: "not found"
+        })
+      }
+    }
+  }
 }
 
 export default Api;
---
+++ test/express-test.spec.ts
@@ -0,0 +1,53 @@
+import { Server } from "http"
+import express from "Express";
+import Api from "../index";
+import fetch from "node-fetch";
+import join from "url-join";
+const getPort= require("get-port-sync");
+import path from "path";
+
+describe("Test Api",()=>{
+
+    let server:Server;
+    const app = express();
+    const port = getPort();
+    let endpoint = `http://localhost:${port}`;
+    function url(urlPath:string){
+        return join(endpoint,urlPath);
+    }
+    beforeAll((async)=>{
+        server = app.listen(port,async);
+        
+        app.use("/hello",Api.fold(Api.get((req,res)=>{
+            return {message:"hello"};
+        })));
+    })
+
+
+    afterAll(()=>{
+        server.close();
+    })
+
+
+
+    it("should be able to create a hello world get request",async ()=>{
+
+        const endpoint = url("/hello");
+        const result = await (await fetch(endpoint)).json();
+        expect(result.message).toBe("hello")
+
+    });
+
+
+    it("should not be able to create a hello world post request",async ()=>{
+
+        const endpoint = url("/hello");
+        const result = await (await fetch(endpoint,{
+            method:"post"
+        })).text();
+        console.log(result);
+        //expect(result.message).toBe("hello")
+
+    });
+
+})
